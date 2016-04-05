e328eaadb82a5119f548ac037c6bd6239d644a26
Martin Vondrák <vondrm12@fit.cvut.cz>
2016-04-05 12:19:04 +0200

    Zavedeni composeru do projektu. Instalace a konfigurace Twigu.

--- autoload.php
+++ autoload.php
@@ -6,7 +6,8 @@
  * Time: 18:51
  */
 
-spl_autoload_register(function ($class_name) {
-    $class_name = str_replace('\\', '/', $class_name);
-    include_once $class_name . '.php';
-});
+require __DIR__ . '/vendor/autoload.php';
+
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/src/Views');
+$twig = new Twig_Environment($loader);
